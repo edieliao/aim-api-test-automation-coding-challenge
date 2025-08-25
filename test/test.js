61f30f910180c5bed5c2c0852560c5c09c4ab3f3
Edie Liao <edie.liao.dev@gmail.com>
2025-08-24 20:07:37 -0500

    Added API acceptance test methods and outline

--- test/test.js
+++ test/test.js
@@ -7,13 +7,31 @@ chai.use(chaiHttp);
 
 const api = request.execute(process.env.API_URL);
 
-describe("API", () => {
-    describe("GET", () => {
+describe("SKU API Acceptance Tests", () => {
+    describe("Create", () => {
+        it("should work", () => {
+            expect.fail("TODO: Test WIP");
+        });
+    });
+
+    describe("Read", () => {
         it("should work", () => {
             api.get("/")
             .end((err, response) => {
                 expect(response).to.have.status(200);
             });
+        });
+    });
+
+    describe("Update", () => {
+        it("should work", () => {
+            expect.fail("TODO: Test WIP");
+        });
+    });
+
+    describe("Delete", () => {
+        it("should work", () => {
+            expect.fail("TODO: Test WIP");
         });
     });
 });
